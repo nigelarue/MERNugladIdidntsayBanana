42b3614bab825cf7b6322b771d4cbeb59dc67561
nigelarue <larue.nigel91@gmail.com>
2023-02-16 16:56:35 -0600

    updated connections to mongodb in donnections.js, and added 'graphql': '^16.6.0', dependency to client package.json.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
+
+// uses the mongoose module to connect to a MongoDB database. It connects to the database specified in the MONGODB_URI environment variable, or to the default database mongodb://127.0.0.1:27017/googlebooks if the environment variable is not defined.
+
+// The useNewUrlParser and useUnifiedTopology options are passed to the mongoose.connect() method to ensure that the MongoDB Node.js driver can use the latest and most stable connection string parser and topology engine.
