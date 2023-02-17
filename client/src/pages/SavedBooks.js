8f173063ce686578677bd70b7e96b7af8f2c8833
nigelarue <larue.nigel91@gmail.com>
2023-02-16 18:35:19 -0600

    updated savedBooks.js in pages to remove hook and use QUERY_ME query and REMOVE_BOOK mutation.

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button, } from 'react-bootstrap';
+// import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 // import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
